0b34ad8f11d0909a0047f9b7c4c85a914b8b2f87
Ryan Downs <rdowns@us.ibm.com>
2017-07-30 12:28:02 -0500

    style About page and link to resources

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -6,7 +6,16 @@ var About = (props) =>{
   return (
     <div>
       <h1 className="text-center">Welcome to the About page</h1>
-      <p>This is a small test app that uses the Open Weather API to display the weather of a city via search</p>
+      <p>This is a weather application built on React. I have built this for The Complete React Web App Developer Course.</p>
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   );
 };
