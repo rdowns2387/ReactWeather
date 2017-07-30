123f74fe42c2f9806677b260f07fbc6ed4649c3b
Ryan Downs <rdowns@us.ibm.com>
2017-07-30 12:20:23 -0500

    Added the grid

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -5,8 +5,8 @@ var React = require('react');
 var About = (props) =>{
   return (
     <div>
-      <h4>Welcome to the About page</h4>
-      <p>This is a small test app that uses the Open Weather API to display the weather of a city via search</p>      
+      <h1 className="text-center">Welcome to the About page</h1>
+      <p>This is a small test app that uses the Open Weather API to display the weather of a city via search</p>
     </div>
   );
 };
--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -1,11 +1,19 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h4>Examples</h4>
-      <p>Welcome to the Examples page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Austin'>Austin, TX</Link>
+        </li>
+        <li>
+          <Link to='/?location=Seattle'>Seattle, WA</Link>
+        </li>
+      </ol>
     </div>
   );
 }
